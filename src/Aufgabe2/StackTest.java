48db3af679c82e6ea3fa12b53dc1c177a4cd32eb
Redbull <76742406+CaptivatingRedbull@users.noreply.github.com>
2023-12-18 12:18:04 +0100

    Uebung8

--- src/Aufgabe2/StackTest.java
+++ src/Aufgabe2/StackTest.java
@@ -16,7 +16,9 @@ class Book {
 }
 
 class Stack {
-    private final Book[] array;
+    Book x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16;
+    Book [] array;
+
     private int top = 0;
 
     public Stack(int nr) {
@@ -55,4 +57,5 @@ public class StackTest {
             element.print();
         }
     }
+
 }
